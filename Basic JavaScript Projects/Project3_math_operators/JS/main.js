c59d9a46cdfac83818e88636bc23661121ade79c
Keen L <meisterkeen@gmail.com>
2021-02-10 23:57:24 -0800

    add math operations practice

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,24 @@
+function addition() {
+    var twoPlusTwo = 2 + 2;
+    document.getElementById("math").innerHTML = "2 + 2 = " + twoPlusTwo;
+}
+
+function subtraction() {
+    var fiveMinusTwo = 5 - 2;
+    document.getElementById("moreMath").innerHTML = "5 - 3 = " + fiveMinusTwo;
+}
+
+function multiplication() {
+    var sixTimesEight = 6 * 8;
+    document.getElementById("evenMoreMath").innerHTML = "6 * 8 = " + sixTimesEight;
+}
+
+function division() {
+    var backwardsMultiply = 48 / 6;
+    document.getElementById("stillMoreMath").innerHTML = "48 / 6 = " + backwardsMultiply;
+}
+
+function bunchaMath() {
+    var shitTonOMath = (5 + 3) * 10 / 18;
+    document.getElementById("ohGodTooMuchMath").innerHTML = "(5 + 3) * 10 / 18 = " + shitTonOMath;
+}
