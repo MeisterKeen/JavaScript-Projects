eb62cf6943b8353ec388b3049f90b1ace2711119
Keen L <meisterkeen@gmail.com>
2021-02-12 15:50:29 -0800

    More practice with dictionaries

--- Basic JavaScript Projects/Project4_/JS/main.js
+++ Basic JavaScript Projects/Project4_/JS/main.js
@@ -1,13 +1,14 @@
 function my_Dictionary() {
-    var Animal = {
-        Species:"Dog",
+    var Animal = { // Note on syntax: the dictionary is a "var",
+        Species:"Dog", // with listed Key:Value pairs inside
         Color:"Black",
         Breed:"Labrador",
         Age:5,
         Sound:"Bark!",
     };
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
-}
+}  // Note on syntax-- dictionary title DOT key, returns value
+
 
 
 function bad_Dictionary() {
@@ -22,3 +23,23 @@ function bad_Dictionary() {
     delete Car.Year; //deleting the "Year" key removes both values, returns "undefined"
     document.getElementById("myCar").innerHTML = Car.Year;
 }
+
+function datatype() {
+    document.getElementById("ofType").innerHTML = (typeof "Car.Model");
+} // typeof will return the data type of the variable or value asked
+
+function blackHole() {
+    document.getElementById("geezRick").innerHTML = 0/0;
+} // Dividing by zero returns NaN (Not a Number)
+
+function is7ANumber() {
+    var isit = "seven";
+    var itis = isNaN(isit)
+    document.getElementById("numero").innerHTML = itis
+} // isNaN asks if the data is a number.
+
+document.write(2E310)
+//To infinity... AND BELOW!
+document.write(-2E310)
+// 2E310 or its negative is beyond the number limit of Javascript.
+// Is there a hacky way to get around that limit? Why would you ever need to?
