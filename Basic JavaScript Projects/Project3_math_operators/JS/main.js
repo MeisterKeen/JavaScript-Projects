0702d6cfe0f97663649fb4cc28fd0a1798153551
Keen L <meisterkeen@gmail.com>
2021-02-11 23:14:48 -0800

    add more math functions to practice project

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -22,3 +22,33 @@ function bunchaMath() {
     var shitTonOMath = (5 + 3) * 10 / 18;
     document.getElementById("ohGodTooMuchMath").innerHTML = "(5 + 3) * 10 / 18 = " + shitTonOMath;
 }
+
+function modulate() {
+    var modulatory = (25 % 6);
+    document.getElementById("mathapalooza").innerHTML = "25 % 6 r= " + modulatory;
+}
+
+function negate() {
+    var x = 10;
+    document.getElementById("mathalympics").innerHTML = "The negation of " + x + " is " + -x;
+}
+
+function increment() {
+    var z = 5;
+    z++;
+    document.getElementById("mathgate").innerHTML = "5 incremented once is " + z;
+}
+
+function decrement() {
+    var c = 5.25;
+    c--;
+    document.getElementById("mathament").innerHTML = "5.25 decremented once is " + c;
+}
+
+function randomize() {
+    document.getElementById("randomath").innerHTML = (Math.random() * 100);
+}
+
+function pime() {
+    document.getElementById("piemath").innerHTML = (Math.PI);
+}
